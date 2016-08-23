250a39c9222f950a1ccf27a66d48b4e8b73c27a5
fits91 <toha.fits@gmail.com>
2016-08-23 22:15:50 +0300

    change restController

--- Dao/src/main/java/ua/org/fits/config/Contr.java
+++ Dao/src/main/java/ua/org/fits/config/Contr.java
@@ -1,21 +1,19 @@
 package ua.org.fits.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import ua.org.fits.dao.DaoInterBank;
 
 /**
  * Created by developer on 04.08.2016.
  */
-@Controller
+@RestController
 public class Contr {
     @Autowired
     DaoInterBank daoInterBank;
 
     @RequestMapping("/testdb")
-    @ResponseBody
     String testdb() {
 
         return daoInterBank.getCur();
